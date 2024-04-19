504334237170d2dba8bb4e81c1ab7ab7a754ac6a
Alexandre de Lima Konrath <160286787+Alexandre-Konrath@users.noreply.github.com>
2024-04-19 11:26:32 -0300

    removing checked

--- src/components/FiltroTecnologia/index.tsx
+++ src/components/FiltroTecnologia/index.tsx
@@ -15,9 +15,7 @@ export default function Filtro(props: FiltroProps) {
       <div className='filtro'>
         <button onClick={() => handleClick(1)}> React</button>
         <button onClick={() => handleClick(3)}> JavaScript</button>
-        <button onClick={() => handleClick(0)} defaultChecked>
-          Todos
-        </button>
+        <button onClick={() => handleClick(0)}> Todos</button>
         <button onClick={() => handleClick(2)}> Html e Css</button>
       </div>
     </Container>
